5ff1129c97ea64d246bdd1f3049b279727bea5fd
Patrycjusz Figaś <patrycjusz94@gmail.com>
2023-09-15 18:52:19 +0200

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const items = [];
 
 console.log(galleryItems);
+
+galleryItems.forEach(element => {
+    const link = document.createElement('a');
+    link.className = 'gallery__link';
+    link.href = element.original;
+    const image = document.createElement('img');
+    image.className = 'gallery__image';
+    image.src = element.preview;
+    image.setAttribute('title', element.description);
+    image.alt = element.description;
+    link.append(image);
+    items.push(link)
+
+})
+
+gallery.append(...items)
+
+new SimpleLightbox('gallery a', {
+    captionDelay: 250,
+})
